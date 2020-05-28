21ea9875e08e303cdf9e19d65b69762e71bb3e19
Miguel Ccantuta <mccantuta@yahoo.com>
2020-05-28 16:15:42 -0500

    Learning

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -10,16 +10,46 @@ function Descripcion(props) {
   return <p>Esto es la descripcion del sistema</p>;
 }
 
+class Boton extends React.Component {
+  render() {
+    return <button onClick={this.lanzarEvento}>Evento</button>
+  }
+
+  lanzarEvento() {
+    alert('Lanzando evento');
+  }
+}
+
+class Hola extends React.Component {
+  render() {
+    return <h3>Hola</h3>
+  }
+}
+
+class Chao extends React.Component {
+  render() {
+    return <h3>Chao</h3>
+  }
+}
+
+function MensajePersonal(props) {
+  if (props.flag) {
+    return <Hola />
+  }
+  return <Chao />
+}
+
 function App() {
   return (
     <div>
       <Header name="2019" />
       <Descripcion />
+      <Boton />
+      <MensajePersonal flag={false} />
     </div>
   );
 }
 
-const element = <Header name="Test" />;
 ReactDOM.render(
   <App />,
   document.getElementById('root')
