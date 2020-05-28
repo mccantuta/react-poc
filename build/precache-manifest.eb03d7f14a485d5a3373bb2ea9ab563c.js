3f9886b1d2208e611556ad336e34f03a54113161
Miguel Ccantuta <mccantuta@Miguels-MacBook-Pro.local>
2020-05-27 20:56:01 -0500

    Initial commit

---
+++ build/precache-manifest.eb03d7f14a485d5a3373bb2ea9ab563c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66744d77f37ae25259062338906bb38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "678c7d49c58e1281d763",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "74f5c7668f0d0fc48990",
+    "url": "/static/js/2.605dae44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.605dae44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678c7d49c58e1281d763",
+    "url": "/static/js/main.c71ffa0c.chunk.js"
+  },
+  {
+    "revision": "4e4c9cb4b8b05b14918d",
+    "url": "/static/js/runtime-main.73a1925f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
